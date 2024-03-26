9cedafa23db95ed3eeddcbd850e512e8f34f88ba
robudexIT <103070901+robudexIT@users.noreply.github.com>
2024-03-27 03:48:44 +0800

    Pullrequest (#9)
    * Update .github/workflows/main.yml
    
    * update .github/workflows/main.yml
    
    * update title index.php
    
    ---------
    
    Co-authored-by: Your Name <rogmer.bulaclac@gmail.com>

--- app/index.php
+++ app/index.php
@@ -18,7 +18,7 @@ function includeDarkThemeCSS() {
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Kodekloud E-Commerce</title>
+        <title> Robudex E-Commerce</title>
 
         <!-- Favicon -->
         <link rel="icon" href="img/favicon.png" type="image/png" />
